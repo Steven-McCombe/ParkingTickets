63cae7dae7a8bab7d425874b1c70a153f352e5c3
Steven McCombe <smccombe93@gmail.com>
2023-11-06 00:18:02 -0500

    added helper function to clean the URL

--- ParkingViolationApp/helpers/cleanURL.js
+++ ParkingViolationApp/helpers/cleanURL.js
@@ -3,13 +3,13 @@
 // Use a regular expression to replace the unwanted part of the URL with an empty string before saving it to the database.
 function cleanSummonsImageUrl(url) {
     if (url.includes('&locationName=_____________________')) {
-    const cleanedUrl = url.replace(/&locationName=_____________________/, '');
-    return cleanedUrl;
-  } else {
-    return url;
+      const cleanedUrl = url.replace(/&locationName=_____________________/, '');
+      return cleanedUrl;
+    } else {
+      return url;
+    }
   }
-
+  
   module.exports = {
     cleanSummonsImageUrl,
   };
-
