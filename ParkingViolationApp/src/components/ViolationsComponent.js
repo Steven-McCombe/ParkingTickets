54afe2c13b014265765267ca28dba0e2acf18056
Steven McCombe <smccombe93@gmail.com>
2023-11-06 01:13:51 -0500

    updated styling. Needs work

--- ParkingViolationApp/src/components/ViolationsComponent.js
+++ ParkingViolationApp/src/components/ViolationsComponent.js
@@ -1,11 +1,17 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, Button, StyleSheet, FlatList, ActivityIndicator } from 'react-native';
+import { View, Text, Button, StyleSheet, FlatList, ActivityIndicator, TouchableOpacity } from 'react-native';
 import { fetchViolations, requestViolationsUpdate } from '../../utils/violationsService';
 
 const ViolationsComponent = ({vehicleId}) => {
   const [violations, setViolations] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [expandedViolationId, setExpandedViolationId] = useState(null);
+
+const toggleExpand = (id) => {
+  setExpandedViolationId(expandedViolationId === id ? null : id);
+};
+
 
   const getViolationsData = async () => {
     try {
@@ -37,7 +43,21 @@ const ViolationsComponent = ({vehicleId}) => {
 
   const renderItem = ({ item }) => (
     <View style={styles.violationItem}>
-      <Text>{item.summonsNumber} - {item.issueDate}</Text> 
+      <TouchableOpacity onPress={() => toggleExpand(item._id)}>
+        <View>
+          <Text style={styles.violationTitle}>{item.summonsNumber}</Text>
+          <Text>{new Date(item.issueDate).toLocaleDateString()}</Text>
+          {expandedViolationId === item._id && (
+            <View style={styles.violationDetails}>
+              {/* Include all relevant details here */}
+              <Text>Plate: {item.plate}</Text>
+              <Text>Violation: {item.violation}</Text>
+              <Text>Violation Time: {new Date(item.violationTime).toLocaleTimeString()}</Text>
+              {/* ... other details ... */}
+            </View>
+          )}
+        </View>
+      </TouchableOpacity>
     </View>
   );
   
@@ -62,13 +82,34 @@ const styles = StyleSheet.create({
     padding: 20,
   },
   violationItem: {
+    backgroundColor: '#fff',
     padding: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: '#eee',
+    marginVertical: 8,
+    borderRadius: 5,
+    shadowOpacity: 0.1,
+    shadowRadius: 3,
+    shadowColor: '#000',
+    shadowOffset: { height: 0, width: 0 },
+    elevation: 1,
+  },
+  violationTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  violationDetails: {
+    marginTop: 10,
+    paddingTop: 10,
+    borderTopWidth: 1,
+    borderTopColor: '#eee',
+    // Include any additional styling for the details section
   },
   error: {
     color: 'red',
   },
+  // ... any other styles you need ...
 });
+
+
+
 
 export default ViolationsComponent;
