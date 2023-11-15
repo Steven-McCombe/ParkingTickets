4763794db510fdb92d604db1823ddbd9080a70f3
Steven McCombe <smccombe93@gmail.com>
2023-11-14 23:15:05 -0500

    working ticket viewer

--- ParkingViolationApp/src/components/PDFViewerModal.js
+++ ParkingViolationApp/src/components/PDFViewerModal.js
@@ -1,65 +1,56 @@
-import React from 'react';
-import { Modal, View, TouchableOpacity, Text, StyleSheet } from 'react-native';
-import Pdf from 'react-native-pdf';
-
-const PDFViewerModal = ({ isVisible, onClose, pdfUrl }) => {
-  console.log("PDF Url is here" , pdfUrl)
-  const source = { uri: pdfUrl.replace("http://", "https://"), cache: true };
-  console.log("Source is here" , source)
-  return (
-    <Modal
-      animationType="slide"
-      transparent={false}
-      visible={isVisible}
-      onRequestClose={onClose}
-    >
-      <View style={styles.modalContainer}>
-        <Text>PDF Viewer {pdfUrl}</Text>
-        <Pdf
-                    source={source}
-                    onLoadComplete={(numberOfPages,filePath) => {
-                        console.log(`Number of pages: ${numberOfPages}`);
-                    }}
-                    onPageChanged={(page,numberOfPages) => {
-                        console.log(`Current page: ${page}`);
-                    }}
-                    onError={(error) => {
-                        console.log(error);
-                    }}
-                    onPressLink={(uri) => {
-                        console.log(`Link pressed: ${uri}`);
-                    }}
-                    style={styles.pdf}/>
-        <TouchableOpacity onPress={onClose} style={styles.closeButton}>
-          <Text style={styles.closeButtonText}>Close</Text>
-        </TouchableOpacity>
-      </View>
-    </Modal>
-  );
-};
-
-const styles = StyleSheet.create({
-  modalContainer: {
-    flex: 1,
-    marginTop: 22,
-  },
-  webview: {
-    flex: 1,
-    width: '100%',
-    height: '100%',
-  },
-  closeButton: {
-    position: 'absolute',
-    top: 30,
-    right: 20,
-    padding: 10,
-    backgroundColor: 'red',
-    borderRadius: 5,
-  },
-  closeButtonText: {
-    color: 'white',
-    fontSize: 16,
-  },
-});
-
-export default PDFViewerModal;
+  import React from 'react';
+  import { Modal, View, TouchableOpacity, Text, StyleSheet } from 'react-native';
+  import { WebView } from 'react-native-webview';
+  import palette from '../styles/colorScheme';
+  
+  const PDFViewerModal = ({ isVisible, onClose, pdfUrl }) => {
+    return (
+      <Modal
+        animationType="slide"
+        transparent={false}
+        visible={isVisible}
+        onRequestClose={onClose}
+      >
+        <View style={styles.modalContainer}>
+          <WebView
+            source={{ uri: pdfUrl.replace("http://", "https://")}}
+            style={styles.webview}
+            onError={(syntheticEvent) => {
+              const { nativeEvent } = syntheticEvent;
+              console.warn('WebView error: ', nativeEvent);
+            }}
+          />
+          <TouchableOpacity onPress={onClose} style={styles.closeButton}>
+            <Text style={styles.closeButtonText}>X</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
+    );
+  };
+  
+  const styles = StyleSheet.create({
+    modalContainer: {
+      flex: 1,
+      marginTop: 22,
+    },
+    webview: {
+      flex: 1,
+      width: '100%',
+      height: '100%',
+    },
+    closeButton: {
+      position: 'absolute',
+      top: 30,
+      right: 20,
+      padding: 10,
+      backgroundColor: palette.danger,
+      borderRadius: 5,
+    },
+    closeButtonText: {
+      color: palette.textPrimary,
+      fontSize: 16,
+    },
+  });
+  
+  export default PDFViewerModal;
+  
